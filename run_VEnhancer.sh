c83309bdb965c1b75da9becdcca6b9abeedbe953
jwhe <hejingwenhejingwen@outlook.com>
2024-08-19 21:55:54 +0800

    Update run_VEnhancer.sh

--- run_VEnhancer.sh
+++ run_VEnhancer.sh
@@ -1,5 +1,3 @@
-export PATH=/mnt/petrelfs/share_data/hejingwen/ffmpeg-4.3.2-amd64-static/:$PATH
-srun -p video-aigc-1 --gres=gpu:1 --job-name=SRI  \
 python enhance_a_video.py \
 --up_scale 4 --target_fps 24 --noise_aug 250 \
 --solver_mode 'fast' --steps 15 \
